a47af40b47fd6fed44a359d82a3dff70c77e0444
Syasdras <71237998+Syasdras@users.noreply.github.com>
2020-11-22 05:13:29 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ProyectoSeminarioAlgoritmia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GraphLayout = new System.Windows.Forms.Panel();
+            this.matriz = new System.Windows.Forms.DataGridView();
+            this.eliminar_Vertice = new System.Windows.Forms.Button();
+            this.Graficar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.agregar_Vertice = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matriz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GraphLayout
+            // 
+            this.GraphLayout.Dock = System.Windows.Forms.DockStyle.Left;
+            this.GraphLayout.Location = new System.Drawing.Point(0, 0);
+            this.GraphLayout.Name = "GraphLayout";
+            this.GraphLayout.Size = new System.Drawing.Size(434, 450);
+            this.GraphLayout.TabIndex = 0;
+            // 
+            // matriz
+            // 
+            this.matriz.AllowUserToAddRows = false;
+            this.matriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matriz.Location = new System.Drawing.Point(452, 12);
+            this.matriz.Name = "matriz";
+            this.matriz.RowHeadersWidth = 47;
+            this.matriz.Size = new System.Drawing.Size(340, 150);
+            this.matriz.TabIndex = 1;
+            this.matriz.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.matriz_CellClick);
+            this.matriz.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.matriz_CellEndEdit);
+            this.matriz.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.matriz_CellFormatting);
+            this.matriz.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.matriz_CellValueChanged);
+            this.matriz.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.matriz_DefaultValuesNeeded);
+            // 
+            // eliminar_Vertice
+            // 
+            this.eliminar_Vertice.Location = new System.Drawing.Point(620, 203);
+            this.eliminar_Vertice.Name = "eliminar_Vertice";
+            this.eliminar_Vertice.Size = new System.Drawing.Size(89, 30);
+            this.eliminar_Vertice.TabIndex = 3;
+            this.eliminar_Vertice.Text = "Eliminar Vertice";
+            this.eliminar_Vertice.UseVisualStyleBackColor = true;
+            // 
+            // Graficar
+            // 
+            this.Graficar.Location = new System.Drawing.Point(516, 239);
+            this.Graficar.Name = "Graficar";
+            this.Graficar.Size = new System.Drawing.Size(193, 30);
+            this.Graficar.TabIndex = 4;
+            this.Graficar.Text = "Crear Grafo";
+            this.Graficar.UseVisualStyleBackColor = true;
+            this.Graficar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(528, 275);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // agregar_Vertice
+            // 
+            this.agregar_Vertice.Location = new System.Drawing.Point(516, 203);
+            this.agregar_Vertice.Name = "agregar_Vertice";
+            this.agregar_Vertice.Size = new System.Drawing.Size(89, 30);
+            this.agregar_Vertice.TabIndex = 6;
+            this.agregar_Vertice.Text = "Agregar Vertice";
+            this.agregar_Vertice.UseVisualStyleBackColor = true;
+            this.agregar_Vertice.Click += new System.EventHandler(this.agregar_Vertice_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(516, 168);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(193, 30);
+            this.reset.TabIndex = 7;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(528, 301);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(528, 327);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "valor";
+            this.label1.TextChanged += new System.EventHandler(this.valor);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Fila";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Columna";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.agregar_Vertice);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Graficar);
+            this.Controls.Add(this.eliminar_Vertice);
+            this.Controls.Add(this.matriz);
+            this.Controls.Add(this.GraphLayout);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.matriz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GraphLayout;
+        private System.Windows.Forms.DataGridView matriz;
+        private System.Windows.Forms.Button eliminar_Vertice;
+        private System.Windows.Forms.Button Graficar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button agregar_Vertice;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSeminarioAlgoritmia
+{
+    public partial class Form1 : Form
+    {
+        char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+        int contador = 0;
+        Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GraphLayout.Controls.Clear();
+            //openChildForm(new grafo());
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+
+            //create the graph content 
+            for (int i = 0; i < contador; i++)
+            {
+                graph.AddNode(az[i].ToString());
+            }
+
+
+            //graph.AddEdge("A", "B");
+            //graph.AddEdge("B", "C");
+            //graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            //graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            //graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            //Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+            //associate the viewer with the form 
+            this.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            GraphLayout.Controls.Add(viewer);
+            GraphLayout.ResumeLayout();
+
+
+        }
+
+        private void agregar_Vertice_Click(object sender, EventArgs e)
+        {
+
+            string name = Convert.ToString(az[contador]);
+            matriz.Columns.Add(name, name);
+            matriz.Rows.Add();
+
+            matriz.Columns[name].DefaultCellStyle.NullValue = "0";
+
+            contador++;
+
+
+        }
+
+        private void matriz_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            this.matriz.Rows[e.RowIndex].HeaderCell.Value = (az[e.RowIndex]).ToString();
+
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            GraphLayout.Controls.Clear();
+            contador = 0;
+        }
+
+        private void matriz_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells["a"].Value = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string searchValue = textBox1.Text;
+            matriz.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            try
+            {
+                bool valueResult = false;
+                foreach (DataGridViewRow row in matriz.Rows)
+                {
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+                        if (row.Cells[i].Value != null && row.Cells[i].Value.ToString().Equals(searchValue))
+                        {
+                            int rowIndex = row.Index;
+                            matriz.Rows[rowIndex].Selected = true;
+                            valueResult = true;
+                            break;
+                        }
+                    }
+
+                }
+                if (!valueResult)
+                {
+                    MessageBox.Show("Unable to find " + textBox1.Text, "Not Found");
+                    return;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void matriz_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (matriz.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                //MessageBox.Show(matriz.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() + "Fila: " + (e.RowIndex + 1) + "Columna" + (e.ColumnIndex + 1));
+                //graph.AddEdge(az[e.RowIndex].ToString(), az[e.ColumnIndex].ToString());
+            }
+        }
+
+        private void matriz_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void matriz_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            string pesostr = "";
+            string newvalue = (string)matriz[e.ColumnIndex, e.RowIndex].Value;
+            textBox1.Text = newvalue;
+            textBox2.Text = (e.RowIndex+1).ToString();
+            textBox3.Text = (e.ColumnIndex+1).ToString();
+            if (textBox1.Text != "1")
+            {
+                pesostr = textBox1.Text;
+            }
+            
+
+            graph.AddEdge(az[e.RowIndex].ToString(),pesostr,az[e.ColumnIndex].ToString());
+        
+        }
+
+        private void valor(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
