7e99ae88b1e40be66ccaed7dbccf849e075ecea6
Syasdras <71237998+Syasdras@users.noreply.github.com>
2020-11-23 20:29:40 -0600

    Update Grafo.cs

--- Grafo.cs
+++ Grafo.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Nombre: Christian Omar Soto Martinez
+Codigo: 215475277
+Seccion:D01
+Fecha de entrega: 29/11/2020
+Materia: Seminario de solucion de problemas de algoritmia
+Practica: Representacion Grafica de un  grafo
+
+Proposito:
+Implementar o crear un software que cree la representación gráfica de un grafo a partir de una matriz de adyacencia
+
+*/
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +27,13 @@ namespace ProyectoSeminarioAlgoritmia
 {
     public partial class Form1 : Form
     {
+        //Creamos una arreglo para asignar nombre a los nodos, columnas y filas de la matriz
         char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+        //Contador para verificar la cantidad de filas y nodos a agregar
         int contador = 0;
+        //variable que sirve para saber si el grafo fue creado
         bool grafocreado = false;
+        //Objeto tipo grafo que se crea
         Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
         public Form1()
         {
@@ -24,22 +43,23 @@ namespace ProyectoSeminarioAlgoritmia
         }
 
 
-
+        //Funcion dedicada al boton "Graficar"
         private void button3_Click(object sender, EventArgs e)
         {
             
+            //No se puede crear el grafo si no se han añadido vertices
                 if (contador != 0)
                 {
+                //No se puede crear el grafo si no se marcar el tipo de grafo que es 
                     if (opc_noponderado.Checked == false && opc_ponderado.Checked == false)
                     {
                         MessageBox.Show("Seleccione el tipo de grafo");
                     }
-
+                    // el else del if anterior
                     else
                     {
                         Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
                         GraphLayout.Controls.Clear();
-                        //create a viewer object 
                         
                         viewer.EdgeInsertButtonVisible = false;
                         viewer.LayoutAlgorithmSettingsButtonVisible = false;
@@ -48,23 +68,11 @@ namespace ProyectoSeminarioAlgoritmia
                         viewer.UndoRedoButtonsVisible = false;
 
                         viewer.NavigationVisible = false;
-                        //create a graph object 
-
-                        //create the graph content 
                         for (int i = 0; i < contador; i++)
                         {
                             graph.AddNode(az[i].ToString());
                         }
-
-                        //graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
-                        //graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
-                        //graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
-                        //Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
-                        //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
-                        //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
-                        //bind the graph to the viewer 
                         viewer.Graph = graph;
-                        //associate the viewer with the form 
                         this.SuspendLayout();
                         viewer.Dock = System.Windows.Forms.DockStyle.Fill;
                         GraphLayout.Controls.Add(viewer);
@@ -73,14 +81,14 @@ namespace ProyectoSeminarioAlgoritmia
 
                     }
                 }
-
+                //Mensaje de error si se intento crear el grafo con una cantidad de vertices 0
                 else
                 {
 
                     MessageBox.Show("No se añadio ningun vertice o no se selecciono tipo de grafo", "Error");
                 }
             
-
+                //Si el grafo fue creado desactivamos los botones menos el de reset
                 if(grafocreado == true)
                 {
                 opc_noponderado.Enabled = false;
@@ -92,6 +100,7 @@ namespace ProyectoSeminarioAlgoritmia
                 }
         }
 
+        //Al añadir un vertice  añadimos nodo,columna y fila
         private void agregar_Vertice_Click(object sender, EventArgs e)
         {
 
@@ -106,16 +115,17 @@ namespace ProyectoSeminarioAlgoritmia
         }
 
 
-
+        //Al presionar reset reiniciamos la applicacion
         private void reset_Click(object sender, EventArgs e)
         {
             Application.Restart();
         }
 
+        //Al eliminar un vertice  eliminarmos nodo,columna y fila
         private void eliminar_Vertice_Click(object sender, EventArgs e)
 
         {
-            
+            //Solo se elimina si el contador el mayor a 1
             if (contador > 1)
             {
                 contador--;
@@ -132,28 +142,28 @@ namespace ProyectoSeminarioAlgoritmia
                 MessageBox.Show("Se debe contar con al menos un Vertice", "Error");
             }
         }
-
+        //Boton de salida de la applicacion
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
+        //Boton para minimizar
         private void pictureBox2_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
         }
-
+        //Asignamos el valor a las columnas
         private void matriz_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
             this.matriz.Rows[e.RowIndex].HeaderCell.Value = (az[e.RowIndex]).ToString();
         }
-
+        //Para tomar los valores que se ingresan a las celdas de la matriz
         private void matriz_CellEndEdit_1(object sender, DataGridViewCellEventArgs e)
         {
             
             string newvalue = (string)matriz[e.ColumnIndex, e.RowIndex].Value;
             string pesostr = newvalue;
-
+            //Si el valor es diferente de nuvo
             if (matriz[e.ColumnIndex, e.RowIndex].Value != null)
             {
                 if (opc_noponderado.Checked == true)
@@ -171,17 +181,12 @@ namespace ProyectoSeminarioAlgoritmia
             }
 
         }
-
+        //Se establece el valor nulo por defecto de la matriz
         private void matriz_DefaultValuesNeeded_1(object sender, DataGridViewRowEventArgs e)
         {
             e.Row.Cells["a"].Value = "0";
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        //Boton que muestra las instrucciones del programa
         private void pictureBox4_Click(object sender, EventArgs e)
         {
             instrucciones instrucciones = new instrucciones();
@@ -192,7 +197,7 @@ namespace ProyectoSeminarioAlgoritmia
 
         int posX;
         int posY;
-
+        //Funcion para arrastrar la ventana
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
                
